43be251a701244fa6d9cb2e393f5e5827b3bc715
Xinx1201 <z975203723@gmail.com>
2021-01-15 13:53:24 +0800

    - 初始化仓库

---
+++ ShareCode.js
@@ -0,0 +1,139 @@
+/*
+ * @Author: ZXG 
+ * @Date: 2020-12-04 13:45:04 
+ * @Last Modified by: ZXG
+ * @Last Modified time: 2021-01-14 14:11:17
+ */
+
+【东东农场】
+// xin
+554359d55fcc4a1fab5f17a946cbf338
+// 宝
+4625d3914d044bacbae685e9d60f14c8
+// ksy
+edf5df477f3e4c45960909255988b345
+// ksyd
+dfb7e6bd0c3649d69d4f61c41a382675
+
+【东东萌宠】
+// xin
+MTE1NDQ5MzYwMDAwMDAwMzkwMTMzNTk=
+// 宝
+MTAxODEyMjkxMDAwMDAwMDQwMDQ5ODU5
+// ksy
+MTE1NDAxNzgwMDAwMDAwNDE2NTU0Nzk=
+// ksyd
+MTE1NDQ5MzYwMDAwMDAwNDI0MDQ1NzM=
+
+【种豆得豆】
+// xin
+mlrdw3aw26j3xtlnoc5rovsdcl364xjx4ctvrhq
+// 宝
+e7lhibzb3zek2volndkiirtldf2etf3cbfqgk5y
+// ksy
+olmijoxgmjutydzvmwfbl6e6l6h2cth3aj3eb4q
+// ksyd
+gou7sxm3hztwog43s4wlioc4zwmle4lerqaqbky
+
+
+【东东工厂】
+// xin
+T0225KkcRR5K9QfSJxL2kPBcdACjVWnYaS5kRrbA
+// 宝
+T0225KkcRB5L81yFIhOmxv4DcwCjVWnYaS5kRrbA
+// ksy
+T0225KkcRkxP8gKGIxvzxfBYIACjVWnYaS5kRrbA
+// ksyd
+T0205KkcBl5qox6mVkix7J5pCjVWnYaS5kRrbA
+
+【京喜工厂】
+// xin
+7cgl2Xm_soDzanE7bzNVdA==
+// 宝
+vuPgt-FKtWwBSRuB_yxNJw==
+// ksy
+t0cC4d00k21nb7mS-skioA==
+// ksyd
+7D55VU2CUlCJ3m_4bd2mJg==
+
+【京东赚赚】
+// xin
+AUWE5mq3DzWYND2j62XsclQ
+// 宝
+AUWE5m63Cyz1aCmmqj3VDkg
+// ksy
+AUWE5mf_GymNZC2H_jHsYwQ
+// ksyd
+AUWE52e3jm395fjK9pRUp
+
+【疯狂的JOY】
+// xin
+4hcydYCP_3SVJFTBkKtsLKt9zd5YaBeE
+// 宝
+qNOtcKihtPlqVT188Rh9_6t9zd5YaBeE
+// ksy
+sRQ2jh_ETvfB8zU3_op5Rqt9zd5YaBeE
+// ksyd
+2AxO9_8nIcYf8ITfyXlpcg==
+
+【签到领现金】
+// xin
+eU9YaezhZq8j9mbXz3RF1g
+// 宝
+eU9YaOzgYPR082eHmXoa0Q
+// ksy
+eU9Yar7kYap38m_SmnRBgg
+// ksyd
+eU9YKqzBMLZXhzyQsxpw
+
+
+//每个月1号，10号，20号凌晨2点清理一次数据库，清理后需重新点击链接提交互助码
+
+
+东东工厂互助码api
+http://api.turinglabs.net/api/v1/jd/ddfactory/create/互助码/
+查看上车人数
+http://api.turinglabs.net/api/v1/jd/ddfactory/count/
+
+
+京喜工厂互助码api
+http://api.turinglabs.net/api/v1/jd/jxfactory/create/互助码/
+查看上车人数
+http://api.turinglabs.net/api/v1/jd/jxfactory/count/
+
+
+种豆提交互助码api
+http://api.turinglabs.net/api/v1/jd/bean/create/互助码/
+查看上车人数
+http://api.turinglabs.net/api/v1/jd/bean/count/
+
+
+农场提交互助码api
+http://api.turinglabs.net/api/v1/jd/farm/create/互助码/
+查看上车人数
+http://api.turinglabs.net/api/v1/jd/farm/count/
+
+
+萌宠提交互助码api
+http://api.turinglabs.net/api/v1/jd/pet/create/互助码/
+查看上车人数
+http://api.turinglabs.net/api/v1/jd/pet/count/
+
+
+京东赚赚小程序互助码api
+https://code.chiang.fun/api/v1/jd/jdzz/create/助力码/
+查看上车人数
+https://code.chiang.fun/api/v1/jd/jdzz/count
+
+疯狂的JOY互助码api
+https://code.chiang.fun/api/v1/jd/jdcrazyjoy/create/助力码/
+查看上车人数
+https://code.chiang.fun/api/v1/jd/jdcrazyjoy/count
+
+京东签到领现金互助码api 
+https://code.chiang.fun/api/v1/jd/jdcash/create/助力码/
+查看上车人数
+https://code.chiang.fun/api/v1/jd/jdcash/count
+
+查看数据库清空时间
+http://api.turinglabs.net/api/v1/jd/cleantimeinfo/
