6c952d87dbd6562e702bcde8bfeccd3baff1f3f9
Xinx1201 <z975203723@gmail.com>
2021-01-16 15:49:27 +0800

    - 添加助力码&TG推送

--- ShareCode.js
+++ ShareCode.js
@@ -2,7 +2,7 @@
  * @Author: ZXG 
  * @Date: 2020-12-04 13:45:04 
  * @Last Modified by: ZXG
- * @Last Modified time: 2021-01-16 15:20:17
+ * @Last Modified time: 2021-01-16 15:48:48
  */
 
 【东东农场】
@@ -15,6 +15,12 @@ edf5df477f3e4c45960909255988b345
 // ksyd
 dfb7e6bd0c3649d69d4f61c41a382675
 
+// 互助码
+554359d55fcc4a1fab5f17a946cbf338@4625d3914d044bacbae685e9d60f14c8@edf5df477f3e4c45960909255988b345@dfb7e6bd0c3649d69d4f61c41a382675
+// TG助力
+/submit_farm_code 554359d55fcc4a1fab5f17a946cbf338&4625d3914d044bacbae685e9d60f14c8&edf5df477f3e4c45960909255988b345&dfb7e6bd0c3649d69d4f61c41a382675
+
+
 【东东萌宠】
 // xin
 MTE1NDQ5MzYwMDAwMDAwMzkwMTMzNTk=
@@ -24,6 +30,12 @@ MTAxODEyMjkxMDAwMDAwMDQwMDQ5ODU5
 MTE1NDAxNzgwMDAwMDAwNDE2NTU0Nzk=
 // ksyd
 MTE1NDQ5MzYwMDAwMDAwNDI0MDQ1NzM=
+
+// 互助码
+MTE1NDQ5MzYwMDAwMDAwMzkwMTMzNTk=@MTAxODEyMjkxMDAwMDAwMDQwMDQ5ODU5@MTE1NDAxNzgwMDAwMDAwNDE2NTU0Nzk=@MTE1NDQ5MzYwMDAwMDAwNDI0MDQ1NzM=
+// TG助力
+/submit_pet_code MTE1NDQ5MzYwMDAwMDAwMzkwMTMzNTk=&MTAxODEyMjkxMDAwMDAwMDQwMDQ5ODU5&MTE1NDAxNzgwMDAwMDAwNDE2NTU0Nzk=&MTE1NDQ5MzYwMDAwMDAwNDI0MDQ1NzM=
+
 
 【种豆得豆】
 // xin
@@ -35,6 +47,12 @@ olmijoxgmjutydzvmwfbl6e6l6h2cth3aj3eb4q
 // ksyd
 gou7sxm3hztwog43s4wlioc4zwmle4lerqaqbky
 
+// 互助码
+mlrdw3aw26j3xtlnoc5rovsdcl364xjx4ctvrhq@e7lhibzb3zek2volndkiirtldf2etf3cbfqgk5y@olmijoxgmjutydzvmwfbl6e6l6h2cth3aj3eb4q@gou7sxm3hztwog43s4wlioc4zwmle4lerqaqbky
+// TG助力
+/submit_bean_code mlrdw3aw26j3xtlnoc5rovsdcl364xjx4ctvrhq&e7lhibzb3zek2volndkiirtldf2etf3cbfqgk5y&olmijoxgmjutydzvmwfbl6e6l6h2cth3aj3eb4q&gou7sxm3hztwog43s4wlioc4zwmle4lerqaqbky
+
+
 【东东工厂】
 // xin
 T0225KkcRR5K9QfSJxL2kPBcdACjVWnYaS5kRrbA
@@ -44,6 +62,12 @@ T0225KkcRB5L81yFIhOmxv4DcwCjVWnYaS5kRrbA
 T0225KkcRkxP8gKGIxvzxfBYIACjVWnYaS5kRrbA
 // ksyd
 T0205KkcBl5qox6mVkix7J5pCjVWnYaS5kRrbA
+
+// 互助码
+T0225KkcRR5K9QfSJxL2kPBcdACjVWnYaS5kRrbA@T0225KkcRB5L81yFIhOmxv4DcwCjVWnYaS5kRrbA@T0225KkcRkxP8gKGIxvzxfBYIACjVWnYaS5kRrbA@T0205KkcBl5qox6mVkix7J5pCjVWnYaS5kRrbA
+// TG助力
+/submit_ddfactory_code T0225KkcRR5K9QfSJxL2kPBcdACjVWnYaS5kRrbA&T0225KkcRB5L81yFIhOmxv4DcwCjVWnYaS5kRrbA&T0225KkcRkxP8gKGIxvzxfBYIACjVWnYaS5kRrbA&T0205KkcBl5qox6mVkix7J5pCjVWnYaS5kRrbA
+
 
 【京东赚赚】
 // xin
@@ -55,6 +79,12 @@ AUWE5mf_GymNZC2H_jHsYwQ
 // ksyd
 AUWE52e3jm395fjK9pRUp
 
+// 互助码
+AUWE5mq3DzWYND2j62XsclQ@AUWE5m63Cyz1aCmmqj3VDkg@AUWE5mf_GymNZC2H_jHsYwQ@AUWE52e3jm395fjK9pRUp
+// TG助力
+【⚠️暂无】
+
+
 【疯狂的JOY】
 // xin
 4hcydYCP_3SVJFTBkKtsLKt9zd5YaBeE
@@ -64,6 +94,12 @@ qNOtcKihtPlqVT188Rh9_6t9zd5YaBeE
 sRQ2jh_ETvfB8zU3_op5Rqt9zd5YaBeE
 // ksyd
 2AxO9_8nIcYf8ITfyXlpcg==
+
+// 互助码
+4hcydYCP_3SVJFTBkKtsLKt9zd5YaBeE@qNOtcKihtPlqVT188Rh9_6t9zd5YaBeE@sRQ2jh_ETvfB8zU3_op5Rqt9zd5YaBeE@2AxO9_8nIcYf8ITfyXlpcg==
+// TG助力
+【⚠️暂无】
+
 
 【签到领现金】
 // xin
@@ -75,6 +111,12 @@ eU9Yar7kYap38m_SmnRBgg
 // ksyd
 eU9YKqzBMLZXhzyQsxpw
 
+// 互助码
+eU9YaezhZq8j9mbXz3RF1g@eU9YaOzgYPR082eHmXoa0Q@eU9Yar7kYap38m_SmnRBgg@eU9YKqzBMLZXhzyQsxpw
+// TG助力
+【⚠️暂无】
+
+
 【口袋书店】
 // xin
 1ca580746847454f851808856db7e565
@@ -85,6 +127,12 @@ eU9YKqzBMLZXhzyQsxpw
 // ksyd
 41c71dfe7059417da809d45cb237c9d0
 
+// 互助码
+1ca580746847454f851808856db7e565@753bbeeac19c447f9358455e444d0de3@9938133d5dc24454ab3a432564fd7e7c@41c71dfe7059417da809d45cb237c9d0
+// TG助力
+/submit_bookshop_code 1ca580746847454f851808856db7e565&753bbeeac19c447f9358455e444d0de3&9938133d5dc24454ab3a432564fd7e7c&41c71dfe7059417da809d45cb237c9d0
+
+
 【京喜工厂】
 // xin
 7cgl2Xm_soDzanE7bzNVdA==
@@ -94,6 +142,12 @@ vuPgt-FKtWwBSRuB_yxNJw==
 t0cC4d00k21nb7mS-skioA==
 // ksyd
 7D55VU2CUlCJ3m_4bd2mJg==
+
+// 互助码
+7cgl2Xm_soDzanE7bzNVdA==@vuPgt-FKtWwBSRuB_yxNJw==@t0cC4d00k21nb7mS-skioA==@7D55VU2CUlCJ3m_4bd2mJg==
+// TG助力
+/submit_jxfactory_code 7cgl2Xm_soDzanE7bzNVdA==&vuPgt-FKtWwBSRuB_yxNJw==&t0cC4d00k21nb7mS-skioA==&7D55VU2CUlCJ3m_4bd2mJg==
+
 
 //每个月1号，10号，20号凌晨2点清理一次数据库，清理后需重新点击链接提交互助码
 
