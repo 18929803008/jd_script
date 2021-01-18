933873dc387e306c5af6c540b0f63cae250c6ad3
Xinx1201 <z975203723@gmail.com>
2021-01-18 15:42:12 +0800

    - 更新互助码

--- ShareCode.js
+++ ShareCode.js
@@ -2,8 +2,26 @@
  * @Author: ZXG 
  * @Date: 2020-12-04 13:45:04 
  * @Last Modified by: ZXG
- * @Last Modified time: 2021-01-16 15:48:48
+ * @Last Modified time: 2021-01-18 15:41:34
  */
+
+
+
+
+ // ——————————————————————————————Turing Lab Bot——————————————————————————————
+
+
+ 【种豆得豆】
+// xin
+mlrdw3aw26j3xtlnoc5rovsdcl364xjx4ctvrhq
+// 宝
+e7lhibzb3zek2volndkiirtldf2etf3cbfqgk5y
+// ksy
+olmijoxgmjutydzvmwfbl6e6l6h2cth3aj3eb4q
+// ksyd
+gou7sxm3hztwog43s4wlioc4zwmle4lerqaqbky
+
+/submit_bean_code mlrdw3aw26j3xtlnoc5rovsdcl364xjx4ctvrhq&e7lhibzb3zek2volndkiirtldf2etf3cbfqgk5y&olmijoxgmjutydzvmwfbl6e6l6h2cth3aj3eb4q&gou7sxm3hztwog43s4wlioc4zwmle4lerqaqbky
 
 【东东农场】
 // xin
@@ -15,9 +33,6 @@ edf5df477f3e4c45960909255988b345
 // ksyd
 dfb7e6bd0c3649d69d4f61c41a382675
 
-// 互助码
-554359d55fcc4a1fab5f17a946cbf338@4625d3914d044bacbae685e9d60f14c8@edf5df477f3e4c45960909255988b345@dfb7e6bd0c3649d69d4f61c41a382675
-// TG助力
 /submit_farm_code 554359d55fcc4a1fab5f17a946cbf338&4625d3914d044bacbae685e9d60f14c8&edf5df477f3e4c45960909255988b345&dfb7e6bd0c3649d69d4f61c41a382675
 
 
@@ -31,106 +46,7 @@ MTE1NDAxNzgwMDAwMDAwNDE2NTU0Nzk=
 // ksyd
 MTE1NDQ5MzYwMDAwMDAwNDI0MDQ1NzM=
 
-// 互助码
-MTE1NDQ5MzYwMDAwMDAwMzkwMTMzNTk=@MTAxODEyMjkxMDAwMDAwMDQwMDQ5ODU5@MTE1NDAxNzgwMDAwMDAwNDE2NTU0Nzk=@MTE1NDQ5MzYwMDAwMDAwNDI0MDQ1NzM=
-// TG助力
 /submit_pet_code MTE1NDQ5MzYwMDAwMDAwMzkwMTMzNTk=&MTAxODEyMjkxMDAwMDAwMDQwMDQ5ODU5&MTE1NDAxNzgwMDAwMDAwNDE2NTU0Nzk=&MTE1NDQ5MzYwMDAwMDAwNDI0MDQ1NzM=
-
-
-【种豆得豆】
-// xin
-mlrdw3aw26j3xtlnoc5rovsdcl364xjx4ctvrhq
-// 宝
-e7lhibzb3zek2volndkiirtldf2etf3cbfqgk5y
-// ksy
-olmijoxgmjutydzvmwfbl6e6l6h2cth3aj3eb4q
-// ksyd
-gou7sxm3hztwog43s4wlioc4zwmle4lerqaqbky
-
-// 互助码
-mlrdw3aw26j3xtlnoc5rovsdcl364xjx4ctvrhq@e7lhibzb3zek2volndkiirtldf2etf3cbfqgk5y@olmijoxgmjutydzvmwfbl6e6l6h2cth3aj3eb4q@gou7sxm3hztwog43s4wlioc4zwmle4lerqaqbky
-// TG助力
-/submit_bean_code mlrdw3aw26j3xtlnoc5rovsdcl364xjx4ctvrhq&e7lhibzb3zek2volndkiirtldf2etf3cbfqgk5y&olmijoxgmjutydzvmwfbl6e6l6h2cth3aj3eb4q&gou7sxm3hztwog43s4wlioc4zwmle4lerqaqbky
-
-
-【东东工厂】
-// xin
-T0225KkcRR5K9QfSJxL2kPBcdACjVWnYaS5kRrbA
-// 宝
-T0225KkcRB5L81yFIhOmxv4DcwCjVWnYaS5kRrbA
-// ksy
-T0225KkcRkxP8gKGIxvzxfBYIACjVWnYaS5kRrbA
-// ksyd
-T0205KkcBl5qox6mVkix7J5pCjVWnYaS5kRrbA
-
-// 互助码
-T0225KkcRR5K9QfSJxL2kPBcdACjVWnYaS5kRrbA@T0225KkcRB5L81yFIhOmxv4DcwCjVWnYaS5kRrbA@T0225KkcRkxP8gKGIxvzxfBYIACjVWnYaS5kRrbA@T0205KkcBl5qox6mVkix7J5pCjVWnYaS5kRrbA
-// TG助力
-/submit_ddfactory_code T0225KkcRR5K9QfSJxL2kPBcdACjVWnYaS5kRrbA&T0225KkcRB5L81yFIhOmxv4DcwCjVWnYaS5kRrbA&T0225KkcRkxP8gKGIxvzxfBYIACjVWnYaS5kRrbA&T0205KkcBl5qox6mVkix7J5pCjVWnYaS5kRrbA
-
-
-【京东赚赚】
-// xin
-AUWE5mq3DzWYND2j62XsclQ
-// 宝
-AUWE5m63Cyz1aCmmqj3VDkg
-// ksy
-AUWE5mf_GymNZC2H_jHsYwQ
-// ksyd
-AUWE52e3jm395fjK9pRUp
-
-// 互助码
-AUWE5mq3DzWYND2j62XsclQ@AUWE5m63Cyz1aCmmqj3VDkg@AUWE5mf_GymNZC2H_jHsYwQ@AUWE52e3jm395fjK9pRUp
-// TG助力
-【⚠️暂无】
-
-
-【疯狂的JOY】
-// xin
-4hcydYCP_3SVJFTBkKtsLKt9zd5YaBeE
-// 宝
-qNOtcKihtPlqVT188Rh9_6t9zd5YaBeE
-// ksy
-sRQ2jh_ETvfB8zU3_op5Rqt9zd5YaBeE
-// ksyd
-2AxO9_8nIcYf8ITfyXlpcg==
-
-// 互助码
-4hcydYCP_3SVJFTBkKtsLKt9zd5YaBeE@qNOtcKihtPlqVT188Rh9_6t9zd5YaBeE@sRQ2jh_ETvfB8zU3_op5Rqt9zd5YaBeE@2AxO9_8nIcYf8ITfyXlpcg==
-// TG助力
-【⚠️暂无】
-
-
-【签到领现金】
-// xin
-eU9YaezhZq8j9mbXz3RF1g
-// 宝
-eU9YaOzgYPR082eHmXoa0Q
-// ksy
-eU9Yar7kYap38m_SmnRBgg
-// ksyd
-eU9YKqzBMLZXhzyQsxpw
-
-// 互助码
-eU9YaezhZq8j9mbXz3RF1g@eU9YaOzgYPR082eHmXoa0Q@eU9Yar7kYap38m_SmnRBgg@eU9YKqzBMLZXhzyQsxpw
-// TG助力
-【⚠️暂无】
-
-
-【口袋书店】
-// xin
-1ca580746847454f851808856db7e565
-// 宝
-753bbeeac19c447f9358455e444d0de3
-// ksy
-9938133d5dc24454ab3a432564fd7e7c
-// ksyd
-41c71dfe7059417da809d45cb237c9d0
-
-// 互助码
-1ca580746847454f851808856db7e565@753bbeeac19c447f9358455e444d0de3@9938133d5dc24454ab3a432564fd7e7c@41c71dfe7059417da809d45cb237c9d0
-// TG助力
-/submit_bookshop_code 1ca580746847454f851808856db7e565&753bbeeac19c447f9358455e444d0de3&9938133d5dc24454ab3a432564fd7e7c&41c71dfe7059417da809d45cb237c9d0
 
 
 【京喜工厂】
@@ -143,59 +59,96 @@ t0cC4d00k21nb7mS-skioA==
 // ksyd
 7D55VU2CUlCJ3m_4bd2mJg==
 
-// 互助码
-7cgl2Xm_soDzanE7bzNVdA==@vuPgt-FKtWwBSRuB_yxNJw==@t0cC4d00k21nb7mS-skioA==@7D55VU2CUlCJ3m_4bd2mJg==
-// TG助力
 /submit_jxfactory_code 7cgl2Xm_soDzanE7bzNVdA==&vuPgt-FKtWwBSRuB_yxNJw==&t0cC4d00k21nb7mS-skioA==&7D55VU2CUlCJ3m_4bd2mJg==
 
 
-//每个月1号，10号，20号凌晨2点清理一次数据库，清理后需重新点击链接提交互助码
+【东东工厂】
+// xin
+T0225KkcRR5K9QfSJxL2kPBcdACjVWnYaS5kRrbA
+// 宝
+T0225KkcRB5L81yFIhOmxv4DcwCjVWnYaS5kRrbA
+// ksy
+T0225KkcRkxP8gKGIxvzxfBYIACjVWnYaS5kRrbA
+// ksyd
+T0205KkcBl5qox6mVkix7J5pCjVWnYaS5kRrbA
+
+/submit_ddfactory_code T0225KkcRR5K9QfSJxL2kPBcdACjVWnYaS5kRrbA&T0225KkcRB5L81yFIhOmxv4DcwCjVWnYaS5kRrbA&T0225KkcRkxP8gKGIxvzxfBYIACjVWnYaS5kRrbA&T0205KkcBl5qox6mVkix7J5pCjVWnYaS5kRrbA
 
 
-东东工厂互助码api
-http://api.turinglabs.net/api/v1/jd/ddfactory/create/互助码/
-查看上车人数
-http://api.turinglabs.net/api/v1/jd/ddfactory/count/
+【口袋书店】
+// xin
+1ca580746847454f851808856db7e565
+// 宝
+753bbeeac19c447f9358455e444d0de3
+// ksy
+9938133d5dc24454ab3a432564fd7e7c
+// ksyd
+41c71dfe7059417da809d45cb237c9d0
+
+/submit_bookshop_code 1ca580746847454f851808856db7e565&753bbeeac19c447f9358455e444d0de3&9938133d5dc24454ab3a432564fd7e7c&41c71dfe7059417da809d45cb237c9d0
 
 
-京喜工厂互助码api
-http://api.turinglabs.net/api/v1/jd/jxfactory/create/互助码/
-查看上车人数
-http://api.turinglabs.net/api/v1/jd/jxfactory/count/
 
 
-种豆提交互助码api
-http://api.turinglabs.net/api/v1/jd/bean/create/互助码/
-查看上车人数
-http://api.turinglabs.net/api/v1/jd/bean/count/
+// ——————————————————————————————Commit Code Bot——————————————————————————————
 
 
-农场提交互助码api
-http://api.turinglabs.net/api/v1/jd/farm/create/互助码/
-查看上车人数
-http://api.turinglabs.net/api/v1/jd/farm/count/
+【签到领现金】
+// xin
+eU9YaezhZq8j9mbXz3RF1g
+// 宝
+eU9YaOzgYPR082eHmXoa0Q
+// ksy
+eU9Yar7kYap38m_SmnRBgg
+// ksyd
+eU9YKqzBMLZXhzyQsxpw
+
+/jdcash eU9YaezhZq8j9mbXz3RF1g&eU9YaOzgYPR082eHmXoa0Q&eU9Yar7kYap38m_SmnRBgg&eU9YKqzBMLZXhzyQsxpw
 
 
-萌宠提交互助码api
-http://api.turinglabs.net/api/v1/jd/pet/create/互助码/
-查看上车人数
-http://api.turinglabs.net/api/v1/jd/pet/count/
+【疯狂的JOY】
+// xin
+4hcydYCP_3SVJFTBkKtsLKt9zd5YaBeE
+// 宝
+qNOtcKihtPlqVT188Rh9_6t9zd5YaBeE
+// ksy
+sRQ2jh_ETvfB8zU3_op5Rqt9zd5YaBeE
+// ksyd
+2AxO9_8nIcYf8ITfyXlpcg==
+
+/jdcrazyjoy 4hcydYCP_3SVJFTBkKtsLKt9zd5YaBeE&qNOtcKihtPlqVT188Rh9_6t9zd5YaBeE&sRQ2jh_ETvfB8zU3_op5Rqt9zd5YaBeE&2AxO9_8nIcYf8ITfyXlpcg==
 
 
-京东赚赚小程序互助码api
-https://code.chiang.fun/api/v1/jd/jdzz/create/助力码/
-查看上车人数
-https://code.chiang.fun/api/v1/jd/jdzz/count
+【京东年货节】
+// xin
+ce0724b6233e488fb47f0cfbd671e16f
+// 宝
+c9e9474cd2b944869859f5bd48468fbd
 
-疯狂的JOY互助码api
-https://code.chiang.fun/api/v1/jd/jdcrazyjoy/create/助力码/
-查看上车人数
-https://code.chiang.fun/api/v1/jd/jdcrazyjoy/count
+/jdnh ce0724b6233e488fb47f0cfbd671e16f&c9e9474cd2b944869859f5bd48468fbd
 
-京东签到领现金互助码api 
-https://code.chiang.fun/api/v1/jd/jdcash/create/助力码/
-查看上车人数
-https://code.chiang.fun/api/v1/jd/jdcash/count
 
-查看数据库清空时间
-http://api.turinglabs.net/api/v1/jd/cleantimeinfo/
+【京东赚赚】
+// xin
+AUWE5mq3DzWYND2j62XsclQ
+// 宝
+AUWE5m63Cyz1aCmmqj3VDkg
+// ksy
+AUWE5mf_GymNZC2H_jHsYwQ
+// ksyd
+AUWE52e3jm395fjK9pRUp
+
+/jdzz AUWE5mq3DzWYND2j62XsclQ&AUWE5m63Cyz1aCmmqj3VDkg&AUWE5mf_GymNZC2H_jHsYwQ&AUWE52e3jm395fjK9pRUp
+
+
+【限时活动 - 炸年兽🧨】
+// xin
+cgxZdTXtIbqJ6l3MDw2r66Q2z8_fRn6MskGPsA-p8eOZCB_PaYVnd1zo5VE
+// 宝
+cgxZdTXtILqI7AabCgz7vSJB3rsfVixh9V0GkaQYbvKHFGxoKCfF9yG_ySo
+// ksy
+cgxZdTXtIuiM7ViYCwSuvrXLuOJjIl9s0ZUiMjQ-rE9NetLAAxCYHAnoqiA
+// ksyd
+cgxZdTXtYvqpvES4flfsl7I4FGuew3E6Up4wkC17XP7U5eavwrPjemQ
+
+/jdnian cgxZdTXtIbqJ6l3MDw2r66Q2z8_fRn6MskGPsA-p8eOZCB_PaYVnd1zo5VE&cgxZdTXtILqI7AabCgz7vSJB3rsfVixh9V0GkaQYbvKHFGxoKCfF9yG_ySo&cgxZdTXtIuiM7ViYCwSuvrXLuOJjIl9s0ZUiMjQ-rE9NetLAAxCYHAnoqiA&cgxZdTXtYvqpvES4flfsl7I4FGuew3E6Up4wkC17XP7U5eavwrPjemQ
